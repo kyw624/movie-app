aaf46d0c4f1519e4537e7839ab342c05adf8a4ec
Y.W KIM <kyw624@gmail.com>
2021-04-09 19:52:56 +0900

    Home.js Hooks 사용한 코드로 변경 및 localStorage 활용한 캐시 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,57 +1,67 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Movie from '../components/Movie';
 import './Home.css';
 
-class Home extends Component {
-  state = {
-    isLoading: true,
-    movies: [],
+function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieList, setMovieList] = useState('[]');
+
+  const getMovies = async () => {
+    const MOVIE_LS = localStorage.getItem('movies');
+    let movieData;
+
+    if (MOVIE_LS) {
+      movieData = JSON.parse(MOVIE_LS);
+      console.log('cache');
+    } else {
+      const {
+        data: {
+          data: { movies },
+        },
+      } = await axios.get(
+        'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+      );
+
+      movieData = movies;
+      localStorage.setItem('movies', JSON.stringify(movies));
+    }
+
+    setMovieList(movieData);
+    setIsLoading(false);
+
+    return { isLoading, movieList };
   };
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
-    );
-    this.setState({ movies, isLoading: false });
-  };
+  useEffect(() => {
+    getMovies();
+  }, []);
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => {
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movieList.map((movie) => {
+            return (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Home;
